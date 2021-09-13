65f711a711c125e4872c3f3937b24d0d5f334b79
Dustin Gearhart <gearhart26@gmail.com>
2021-09-13 00:20:11 -0700

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -1,1 +1,7 @@
+// topic = Token Standards --> video = Assignment – ERC20 //
 
+//ERC20 Token assignment for Ethereum Smart Contract Programming 201
+//assignment is to fill in transfer, approve, and transferFrom functions
+
+
+pragma solidity 0.8.0;
